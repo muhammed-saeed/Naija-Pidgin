1fda193d443e8c4e67d041bd2a16778dbf79fa8f
LST <mohammed.yahia3@gmail.com>
2022-06-11 19:59:03 +0200

    fariseq codes

--- Others Dataset/SA/naijasenti_dataset_v1.0/towards_enrich_sentiment_analysis/csv_to_text.py
+++ Others Dataset/SA/naijasenti_dataset_v1.0/towards_enrich_sentiment_analysis/csv_to_text.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import re
 dev_path =  r"C:\Users\lst\Desktop\Naija-Pidgin\SA\naijasenti_dataset_v1.0\towards_enrich_sentiment_analysis\Nigerian Pidgin Tweets and Sentiment.csv"
 train_path = r"C:\Users\lst\Desktop\Naija-Pidgin\SA\naijasenti_dataset_v1.0\towards_enrich_sentiment_analysis\14000 Nigerian Pidgin Tweets and Sentiments.csv"
 test = r"C:\Users\lst\Desktop\Naija-Pidgin\SA\naijasenti_dataset_v1.0\naijasenti_dataset_v1.0\pidgin\pcm_test.csv"
@@ -19,7 +20,44 @@ print(df_train.head())
 print("#########################")
 print(entire_df.head())
 a = entire_df.values
-with open(r"C:\Users\lst\Desktop\Naija-Pidgin\SA\naijasenti_dataset_v1.0\towards_enrich_sentiment_analysis\senti_text.txt", 'w', encoding="utf-8") as f:
+with open(r"C:\Users\lst\Desktop\Naija-Pidgin\SA\naijasenti_dataset_v1.0\towards_enrich_sentiment_analysis\enrich_senti_text.txt", 'w', encoding="utf-8") as f:
     for line in a:
         f.write(str(line))
         f.write("\n")
+
+
+text_ = ""
+with open(r"C:\Users\lst\Desktop\Naija-Pidgin\SA\naijasenti_dataset_v1.0\towards_enrich_sentiment_analysis\enrich_senti_text.txt", 'r', encoding="utf-8") as fb:
+    text_ = fb.read()
+emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags=re.UNICODE)
+new_text = re.sub(emoji_pattern, " ", text_)
+# # new_text = emoji_pattern.sub(r'', text_) # no emoji
+
+# print(text) # with emoji
+
+def deEmojify(text):
+    regrex_pattern = re.compile(pattern = "["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags = re.UNICODE)
+    return regrex_pattern.sub(r'',text)
+    
+# import emoji
+
+# def extract_emojis(s):
+#   return ''.join(c for c in s if c not in emoji.UNICODE_EMOJI['en'])
+# # new_text = deEmojify(text_)
+# clean_text = extract_emojis(text_)
+
+import re
+clean_text = re.sub(r'[^\x00-\x7f]',r' ',text_)  
+
+with open(r"C:\Users\lst\Desktop\Naija-Pidgin\SA\naijasenti_dataset_v1.0\towards_enrich_sentiment_analysis\clean_enrich_senti_text.txt", 'w', encoding="utf-8") as fb:
+    fb.write(clean_text)
