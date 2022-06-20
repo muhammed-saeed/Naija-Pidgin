b882bf68b3181dd0c73ae9458f6081bbcbed64d9
LST <mohammed.yahia3@gmail.com>
2022-06-20 13:45:56 +0200

    working bash

--- Machine Translation/fairseq_train.sh
+++ Machine Translation/fairseq_train.sh
@@ -1,30 +1,29 @@
 DROPOUT=0.3
-ATTENTION_DROPOUT=0.0
+ATTENTION_DROPOUT=0
 ACTIVATION_DROPOUT=0.3
-EMBEDDING_SIZE=420
+EMBEDDING_SIZE=300
 ENC_FFNN=2048
-ENCODER_LAYERS=5
-ENCODER_ATTENTION_HEADS=6
-DECODER_LAYERS=5
-DECODER_ATTENTION_HEADS=6
+ENCODER_LAYERS=4
+ENCODER_ATTENTION_HEADS=10
+DECODER_LAYERS=4
+DECODER_ATTENTION_HEADS=10
 DEC_FFNN=2048
 EPOCH=550
 BATCH_SIZE=64
-ENCODER_LAYER_DROPOUT= 0.0
+ENCODER_LAYER_DROPOUT=0.0
 DECODER_LAYER_DROPOUT=0.1
-SOURCE_LANGUAGE=pcm
-TARGET_LANGUAGE=en
-LABEL_SMOOTHING=0.4
-SAVE_DIR="/home/VD/cychang/ironside_nmt/420_embed_checkpoints/"
-LABEL_CROSS_ENTROPY="label_smoothed_cross_entropy"
+SOURCE_LANGUAGE=en
+TARGET_LANGUAGE=pcm
+LABEL_SMOOTHING=0.5
+SAVE_DIR=/home/CE/musaeed/ironside_nmt/en_pcm_300_64_checkpoints/
+LABEL_CROSS_ENTROPY=label_smoothed_cross_entropy
 WARMUP_UPDATES=4000
-lEARNING_POLICY = inverse_sqrt
+lEARNING_POLICY=inverse_sqrt
 
-fairseq-train "/home/VD/cychang/ironside_nmt/JW300 with bible/pcm_en.tokenized.pcm-en" \
+fairseq-train "/home/CE/musaeed/ironside_nmt/entire_parrellel/en_pcm.tokenized.en-pcm" \
     --arch transformer \
     --dropout $DROPOUT \
-    --attention-dropout $ATTENTION_DROPOUT \ 
-    --encoder-embed-dim   $EMBEDDING_SIZE\
+    --attention-dropout 0     --encoder-embed-dim $EMBEDDING_SIZE \
     --encoder-ffn-embed-dim $ENC_FFNN  \
     --encoder-layers $ENCODER_LAYERS  \
     --encoder-attention-heads $ENCODER_ATTENTION_HEADS \
@@ -38,20 +37,13 @@ fairseq-train "/home/VD/cychang/ironside_nmt/JW300 with bible/pcm_en.tokenized.p
     --optimizer adam \
     --lr 5e-4 \
     --batch-size $BATCH_SIZE \
-    --seed 1  \
-    --encoder-layerdrop $ENCODER_LAYER_DROPOUT \
-    --decoder-layerdrop $DECODER_LAYER_DROPOUT \
-    --criterion $LABEL_CROSS_ENTROPY \ 
-    --warmup-updates $WARMUP_UPDATES \
-    --source-lang $SOURCE_LANGUAGE \ 
-    --label-smoothing $LABEL_SMOOTHING \
-    --lr-scheduler $lEARNING_POLICY \ 
-    --save-dir $SAVE_DIR \
-    
+    --seed 1     --encoder-layerdrop $ENCODER_LAYER_DROPOUT     --decoder-layerdrop $DECODER_LAYER_DROPOUT \
+    --criterion $LABEL_CROSS_ENTROPY     --warmup-updates $WARMUP_UPDATES \
+    --source-lang $SOURCE_LANGUAGE     --label-smoothing $LABEL_SMOOTHING \
+    --lr-scheduler $lEARNING_POLICY   --save-dir $SAVE_DIR \
     --find-unused-parameters  \
     --target-lang $TARGET_LANGUAGE \
     --activation-dropout $ACTIVATION_DROPOUT  \
     --ddp-backend=no_c10d \
-    
-    --log-format=json --log-interval=10 2>&1 \
-    --no-epoch-checkpoints   |  tee  "/home/VD/cychang/pcm_en_5_2048_550_64_420_embed_arch_training_log.log"
+    --no-epoch-checkpoints \
+    --log-format=json --log-interval=10 2>&1    |  tee  "/home/CE/musaeed/ironside_nmt/entire_parrellel/en_pcm_4__10_300_2048_550_64_arch_training_log.log"
