f4f13d78d21def222bf8ec0d1b7dd951faec1253
LST <mohammed.yahia3@gmail.com>
2022-07-11 17:08:05 +0200

    sh

---
+++ ROBERTA-base-en/Transformers/Pre_training_from_scratch/requirements.sh
@@ -0,0 +1,9 @@
+python3 -m pip  install torch torchvision torchaudio
+python3 -m pip install transformers
+python3 -m pip uninstall tokenizers
+python3 -m pip install tokenizers
+python3 -m pip install scipy
+python3 -m pip install sklearn
+python3 -m pip install seaborn
+python3 -m pip install pandas
+python3 -m pip install wandb
