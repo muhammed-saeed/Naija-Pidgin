4df747e0e3bbb47840752e52f6a1b42fed6a16f6
LST <mohammed.yahia3@gmail.com>
2022-08-14 16:18:42 +0200

    BT

--- Machine Translation/joint_embeddings/fairseq_interactive.sh
+++ Machine Translation/joint_embeddings/fairseq_interactive.sh
@@ -4,3 +4,4 @@ CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=1,2,3,4,5,6 fairseq-interactive \
 --path "/home/CE/musaeed/ironside/joint_vocab_checkpoints/validation/config_validation_joint_data_bpe_pcm_en_6_l_4_h_256_embedd_64_b_1024_ffnn_checkpoints/checkpoint_last.pt" \
 --buffer-size 1024 --beam 5 --batch-size 128 \
 --skip-invalid-size-inputs-valid-test >>/home/CE/musaeed/back_translation_pcm_en.txt
+#thiis is used to generate data for the BT
