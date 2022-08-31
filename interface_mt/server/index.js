7fb46573b2e92d966ae86a0c8b624f8f0ac95c64
LST <mohammed.yahia3@gmail.com>
2022-08-31 19:19:19 +0200

    posts and backedn

--- interface_mt/server/index.js
+++ interface_mt/server/index.js
@@ -3,12 +3,16 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js"
+
 const app = express()
 // const express = require()
 app.use(bodyParser.json({limit:"30mb", extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}))
 // because we are going to send files and we can probably send requests
 app.use(cors())
+
+app.use("/posts", postRoutes)
 
 const connection_URL = "mongodb+srv://msaeed1:tester1234@cluster0.8zqla.mongodb.net/?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
