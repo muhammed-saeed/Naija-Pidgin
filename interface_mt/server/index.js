69308c30f458a3ec51934a4b0cf25b36ee0640f4
LST <mohammed.yahia3@gmail.com>
2022-08-31 18:26:08 +0200

    posts and backedn

--- interface_mt/server/index.js
+++ interface_mt/server/index.js
@@ -10,5 +10,12 @@ app.use(bodyParser.urlencoded({limit:"30mb", extended:true}))
 // because we are going to send files and we can probably send requests
 app.use(cors())
 
+const connection_URL = "mongodb+srv://msaeed1:tester1234@cluster0.8zqla.mongodb.net/?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000;
 
 
+mongoose.connect(connection_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> app.listen(PORT, () => console.log(`Server running on PORT :${PORT}`)))
+.catch((error) => console.log(error))
+
+// mongoose.set('useFindAndModify', false);
