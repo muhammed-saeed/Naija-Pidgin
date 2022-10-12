71efd95f9ceb0dcc47a474aeb42487ee234d172e
muhammed-saeed <mohammed.yahia3@gmail.com>
2022-10-12 17:33:09 +0200

    more file

--- nmt_only_bible/bible_overlap.py
+++ nmt_only_bible/bible_overlap.py
@@ -1,23 +1,30 @@
 en_path=r"C:\Users\lst\Documents\Naija-Pidgin\nmt_only_bible\train\train.en"
 pcm_path = r"C:\Users\lst\Documents\Naija-Pidgin\nmt_only_bible\train\train.pcm"
+en_jw300 = r"C:\Users\lst\Documents\Naija-Pidgin\JW300 data\jw300.en"
+pcm_jw300 = r"C:\Users\lst\Documents\Naija-Pidgin\JW300 data\jw300.pcm"
+en_treebank = r"C:\Users\lst\Documents\Naija-Pidgin\Treebank_test\testing_Data\en_parrellel.txt"
+pcm_treebank = r"C:\Users\lst\Documents\Naija-Pidgin\Treebank_test\testing_Data\pcm_parrellel.txt"
 
 def to_lower(string_list):
     string_list = [each_string.lower() for each_string in string_list]
     return string_list
 
+def word_reader(pcm_path):
+    with open(pcm_path, 'r', encoding='utf-8') as f:
+         pcm_wordss = f.read().split()
+    return pcm_wordss
 pcm_words = []
 en_words = []
-with open(pcm_path, 'r', encoding='utf-8') as f:
-    pcm_words = f.read().split()
+pcm_words = word_reader(pcm_path)
 
-with open(en_path, 'r', encoding='utf-8') as f:
-    en_words = f.read().split()
+en_words = word_reader(en_path)
 
-# print(words)  # 👉️ ['one', 'one', 'two', 'two', 'three', 'three']
 en_words = to_lower(en_words)
 pcm_words = to_lower(pcm_words)
 pcm_unique_words = set(pcm_words)
 en_unique_words = set(en_words)
+
+
 print(len(pcm_unique_words))  # 👉️ 3
 print(len(en_unique_words))  # 👉️ 3
 
