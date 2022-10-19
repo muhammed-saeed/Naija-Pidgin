57d29b8f5da0f98d0f5f75a46d827e42e2998b33
muhammed-saeed <mohammed.yahia3@gmail.com>
2022-10-19 05:34:30 +0200

    lines

---
+++ Others Dataset/other mono-pcm/bbc_scrapper/lines.py
@@ -0,0 +1,3 @@
+bbc_data = r"C:\Users\lst\Documents\Naija-Pidgin\Others Dataset\other mono-pcm\bbc_scrapper\bbc_scrapped_data.txt"
+lines = open(bbc_data, "r", encoding="utf-8").readlines()
+print(f"lines {len(lines)}")
