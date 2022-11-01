dcc8a5b0cdc7e11a5e90c6de624c091664e7b284
Muhammed Saeed <musaeed@tony-1.coli.uni-saarland.de>
2022-11-01 19:30:36 +0100

    entire back_translation

--- t5_translation/07_back_translation_training.py
+++ t5_translation/07_back_translation_training.py
@@ -9,8 +9,8 @@ logging.basicConfig(level=logging.INFO)
 transformers_logger = logging.getLogger("transformers")
 transformers_logger.setLevel(logging.WARNING)
 # train_df = pd.read_csv("/home/CE/musaeed/t5_translation/backtranslation/pcmreal_enbt/pcmreal_enbt_train.tsv", sep="\t").astype(str)
-train_df = pd.read_csv("/home/CE/musaeed/t5_translation/backtranslation/enreal_pcmbt/enreal_pcmbt_train.tsv", sep="\t").astype(str)
-
+# train_df = pd.read_csv("/home/CE/musaeed/t5_translation/backtranslation/enreal_pcmbt/enreal_pcmbt_train.tsv", sep="\t").astype(str)
+train_df = pd.read_csv("/home/CE/musaeed/t5_translation/backtranslation/both_real_pcm_enreal_pcmbt_train.tsv", sep="\t").astype(str)
 eval_df = pd.read_csv("/home/CE/musaeed/t5_translation/data/tsv/eval.tsv", sep="\t").astype(str)
 
 # train_df["prefix"] = ""
@@ -32,12 +32,12 @@ model_args.overwrite_output_dir = True
 model_args.preprocess_inputs = False
 # model_args.n_gpu = 2
 model_args.num_return_sequences = 1
-model_args.output_dir = "/home/CE/musaeed/t5_translation/back_translation_real_en"
+model_args.output_dir = "/home/CE/musaeed/t5_translation/both_real_en_pcm_back_translation"
 
-model_args.wandb_project = "MT5 PCM-English Real English Back Translation"
+model_args.wandb_project = "MT5 PCM-English Real PCM & English Back Translation"
 # model_dir = "/home/CE/musaeed/t5_translation/output_dir_backup_7_epoch/checkpoint-49960-epoch-8"
 # model = T5Model("mt5", "google/mt5-base", args=model_args, cuda_devices=[3,2])
 # model = T5Model("mt5", "google/mt5-base", args=model_args, cuda_devices=[2])
-model = T5Model("mt5", "google/mt5-base", args=model_args, cuda_devices=[4])
+model = T5Model("mt5", "google/mt5-base", args=model_args, cuda_devices=[6,7])
 
 model.train_model(train_df, eval_data=eval_df)
