68fe2b6122f4921be3fd1fc2d2e05501cb3bc619
Muhammed Saeed <musaeed@tony-1.coli.uni-saarland.de>
2022-11-14 11:04:52 +0100

    t5_from_pretrained encoder

--- t5_translation/02_translation_from_pretrained_pcm_model.py
+++ t5_translation/02_translation_from_pretrained_pcm_model.py
@@ -10,6 +10,7 @@ transformers_logger = logging.getLogger("transformers")
 transformers_logger.setLevel(logging.WARNING)
 
 train_df = pd.read_csv("/home/CE/musaeed/t5_translation/data/tsv/train.tsv", sep="\t").astype(str)
+train_df = pd.read_csv("/home/CE/musaeed/pcm_distillation_en_bt.tsv", sep="\t").astype(str)
 eval_df = pd.read_csv("/home/CE/musaeed/t5_translation/data/tsv/eval.tsv", sep="\t").astype(str)
 
 # train_df["prefix"] = ""
@@ -31,7 +32,7 @@ model_args.overwrite_output_dir = True
 model_args.preprocess_inputs = False
 model_args.n_gpu = 2
 model_args.num_return_sequences = 1
-model_args.output_dir = "/home/CE/musaeed/t5_translation/t5_en_pcm_pretrained_for_translation"
+model_args.output_dir = "/home/CE/musaeed/t5_translation/t5_seq_distillation"
 
 model_args.wandb_project = "T5-EN_BASE PCM-English Translation"
 model_dir = "/home/CE/musaeed/t5_translation/output_dir_backup_7_epoch/checkpoint-49960-epoch-8"
@@ -41,6 +42,6 @@ english_t5_pretrained_on_pcm = "/home/CE/musaeed/t5_pretrained_pcm/pre_training_
 # model = T5Model("mt5", "google/mt5-base", args=model_args, cuda_devices=[2])
 # model = T5Model("mt5", model_dir, args=model_args, cuda_devices=[2])
 
-model = T5Model("t5", english_t5_pretrained_on_pcm, args=model_args, cuda_devices=[3])
+model = T5Model("t5", english_t5_pretrained_on_pcm, args=model_args, cuda_devices=[4])
 
 model.train_model(train_df, eval_data=eval_df)
